00322ea657d9f9766e42609de806e45f72f2f95a
jcirone <Ciwonie@gmail.com>
2021-04-25 12:25:01 -0400

    refactored with oop design in snake.py

--- 6_Snake_Game/main.py
+++ 6_Snake_Game/main.py
@@ -8,19 +8,24 @@ Start by planning the shape of the snake! Start with 3 squares, approx. 20px.
 3. create snake food            7. detect collision with tail
 4. Detect collision with food
 """
-# GOAL 1 COMPLETE 20 APR 21 WITH OOP
+# GOAL 1 COMPLETE 20 APR 21
 # GOAL 2 COMPLETE 20 APR 21
+# GOAL 3 COMPLETE 20 APR 21
+# GOAL 4 COMPLETE 21 APR 21
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
-screen.setup(width=1200, height=1200)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -30,8 +35,12 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.05)
+    time.sleep(0.1)
     snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
