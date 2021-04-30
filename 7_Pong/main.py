d689270da501d362e83d40c87f8edfa6a93a5671
jcirone <Ciwonie@gmail.com>
2021-04-30 12:00:22 -0400

    ball can detect collision with paddles and walls.

--- 7_Pong/main.py
+++ 7_Pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -7,19 +9,40 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-p1 = Paddle((350, 0))
-p2 = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+pong = Ball()
 
 screen.listen()
-screen.onkey(p1.go_up, "Up")
-screen.onkey(p1.go_down, "Down")
-screen.onkey(p2.go_up, "w")
-screen.onkey(p2.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    pong.move()
+
+    # detect collision with walls (600x800)
+    if pong.ycor() > 280 or pong.ycor() < -270:
+        pong.bounce_y()
+
+    # detect collision with r_paddle
+    if pong.distance(r_paddle) < 50 and pong.xcor() > 320 or pong.distance(l_paddle) < 50 and pong.xcor() < -320:
+        pong.bounce_x()
+
+    # detect R paddle misses
+    if pong.xcor() > 380:
+        pong.reset_position()
+        pong.bounce_x()
+
+    # detect L paddle misses
+    if pong.xcor() < -390:
+        pong.reset_position()
+        pong.bounce_x()
 
 
 screen.exitonclick()
