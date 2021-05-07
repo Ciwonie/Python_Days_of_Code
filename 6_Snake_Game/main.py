4ea551c9bf8ea4659d8004ce3822d765a5180311
jcirone <Ciwonie@gmail.com>
2021-05-07 19:11:41 -0400

    added save data functionality to the snake game! And encoded the information to a .txt.

--- 6_Snake_Game/main.py
+++ 6_Snake_Game/main.py
@@ -18,47 +18,49 @@ from snake import Snake
 from food import Food
 import time
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
 
-snake = Snake()
-food = Food()
-score = Scoreboard()
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    snake = Snake()
+    food = Food()
+    score = Scoreboard()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-    # detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        score.increase_score()
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -280:
-        score.reset()
-        snake.reset()
+        # detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            score.increase_score()
 
-    # detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+        # detect collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -280:
             score.reset()
             snake.reset()
 
+        # detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                score.reset()
+                snake.reset()
 
-screen.exitonclick()
+    screen.exitonclick()
 
 
-
+if __name__ == "__main__":
+    main()
--- 6_Snake_Game/scoreboard.py
+++ 6_Snake_Game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import pickle
 
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
@@ -6,12 +7,19 @@ X_SCOREBOARD = 0
 Y_SCOREBOARD = 260
 SCOREBOARD_COLOR = "white"
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open(file="data.txt", mode="rb") as HIGHEST:
+            try:
+                msg = pickle.load(HIGHEST)
+                self.high_score = int(msg)
+            except Exception as e:
+                self.high_score = 0
+
         self.color(SCOREBOARD_COLOR)
         self.hideturtle()
         self.penup()
@@ -26,13 +34,21 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
+    def update_high_score(self):
+        try:
+            with open(file="data.txt", mode="wb") as HIGHEST:
+                msg = pickle.dumps(str(self.high_score))
+                HIGHEST.write(msg)
+        except Exception as e:
+            print(e)
+
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.update_high_score()
         self.score = 0
         self.update_scoreboard()
 
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
